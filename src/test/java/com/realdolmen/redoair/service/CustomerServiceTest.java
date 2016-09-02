befe66da71cdc15757b12e8047e579b48ac896b0
Nick Hanot <nick.hanot@realdolmen.com>
2016-09-02 13:38:29 +0200

    landingpage fixed - finally

---
+++ src/test/java/com/realdolmen/redoair/service/CustomerServiceTest.java
@@ -0,0 +1,44 @@
+package com.realdolmen.redoair.service;
+
+import com.realdolmen.redoair.domain.Customer;
+import com.realdolmen.redoair.repository.CustomerRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerServiceTest {
+    @InjectMocks
+    private CustomerService service;
+
+    @Mock
+    private CustomerRepository repo;
+    private List<Customer> customers;
+    private Customer customer;
+    private Long id;
+
+    @Before
+    public void setUp() {
+        customers = new ArrayList<>();
+    }
+
+    @Test
+    public void getAllCustomerReturnsListReturnedByRepo() {
+        Mockito.when(repo.findAll()).thenReturn(customers);
+        Assert.assertSame(customers, service.getAllCustomers());
+    }
+
+    @Test
+    public void getCustomerReturnsItemReturnedByRepo() {
+        Mockito.when(repo.findById(id)).thenReturn(customer);
+        Assert.assertSame(customer, service.getCustomerById(id));
+    }
+}
